28e4266a0fdd8ad1b82ec0e634bd89087bd7803c
zwq <zhangwenq053@163.com>
2019-09-15 19:09:20 +0800

    修复空目录名

--- build_dic.py
+++ build_dic.py
@@ -5,6 +5,8 @@ def nested_dict_builder(path_lst):
         lst_trend = lst_base
         dir_list = path.split('/')
         for dir_name in dir_list:
+            if not dir_name:
+                continue
             for trend_dic in lst_trend:
                 if dir_name == trend_dic.get('name'):
                     lst_trend = trend_dic['children']
@@ -24,7 +26,27 @@ def nested_dict_builder(path_lst):
 if __name__ == '__main__':
     lst = ['1/2/3/4', '1/2/22/33/4/55', '1/33/5/6/', '1/2/5/77/9', '1/4/6', '1/33/9/10']
     res = nested_dict_builder(lst)
-    print(res)
+    print(res[0])
 
 
-
+# result
+ss = {'name': '1',
+      'children': [
+          {'name': '2', 'children': [
+              {'name': '3', 'children': [
+                  {'name': '4', 'children': [{}]}]},
+              {'name': '22', 'children': [
+                  {'name': '33', 'children': [
+                      {'name': '4', 'children': [
+                          {'name': '55', 'children': [{}]}]}]}]},
+              {'name': '5', 'children': [
+                  {'name': '77', 'children': [
+                      {'name': '9', 'children': [{}]}]}]}]},
+          {'name': '33', 'children': [
+              {'name': '5', 'children': [
+                  {'name': '6', 'children': [{}]}]}]},
+          {'name': '4', 'children': [
+              {'name': '6', 'children': [{}]}]},
+          {'name': '33', 'children': [
+              {'name': '9', 'children': [
+                  {'name': '10', 'children': [{}]}]}]}]}
