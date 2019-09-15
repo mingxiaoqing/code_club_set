f67e47003248b96e64b6e6d0fb79aeba0d8fe19c
zwq <zhangwenq053@163.com>
2019-09-15 19:16:09 +0800

    修复空目录名

--- build_dic.py
+++ build_dic.py
@@ -1,32 +1,32 @@
-def nested_dict_builder(path_lst):
+def nested_dict_builder(file_path, lst_base):
     """字典嵌套--构造目录树层级"""
-    lst_base = [{}]
-    for path in path_lst:
-        lst_trend = lst_base
-        dir_list = path.split('/')
-        for dir_name in dir_list:
-            if not dir_name:
-                continue
-            for trend_dic in lst_trend:
-                if dir_name == trend_dic.get('name'):
+
+    lst_trend = lst_base
+    dir_list = file_path.split('/')
+    for dir_name in dir_list:
+        if not dir_name:
+            continue
+        for trend_dic in lst_trend:
+            if dir_name == trend_dic.get('name'):
+                lst_trend = trend_dic['children']
+            else:
+                if not trend_dic:
+                    trend_dic['name'] = dir_name
+                    trend_dic['children'] = [{}]
                     lst_trend = trend_dic['children']
                 else:
-                    if not trend_dic:
-                        trend_dic['name'] = dir_name
-                        trend_dic['children'] = [{}]
-                        lst_trend = trend_dic['children']
-                    else:
-                        lst_trend.append({'name': dir_name, 'children': [{}]})
-                        lst_trend = lst_trend[-1]['children']
-                break
-    return lst_base
+                    lst_trend.append({'name': dir_name, 'children': [{}]})
+                    lst_trend = lst_trend[-1]['children']
+            break
 
 
 
 if __name__ == '__main__':
     lst = ['1/2/3/4', '1/2/22/33/4/55', '1/33/5/6/', '1/2/5/77/9', '1/4/6', '1/33/9/10']
-    res = nested_dict_builder(lst)
-    print(res[0])
+    lst_base = [{}]
+    for path in lst:
+        nested_dict_builder(path, lst_base)
+    print(lst_base)
 
 
 # result
